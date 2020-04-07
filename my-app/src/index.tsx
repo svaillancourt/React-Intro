58cbf4f4be2414ddc5ae4b30ea84e022528a135d
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-04-07 16:50:39 -0600

    typescript updates

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -7,8 +7,6 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <App />
-    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
